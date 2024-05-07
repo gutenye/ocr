f1e17da380feb3365393cf1421efe28013213eeb
Guten Ye <ywzhaifei@gmail.com>
2024-05-07 22:10:07 +0800

    FileUtils

--- examples/react-native/src/ocr-react-native/FileUtils.ts
+++ examples/react-native/src/ocr-react-native/FileUtils.ts
@@ -1,5 +1,8 @@
 import { FileUtilsBase } from '@gutenye/ocr-common'
+import { readAsStringAsync } from 'expo-file-system'
 
 export class FileUtils extends FileUtilsBase {
-  static async read(path: string) {}
+  static async read(path: string) {
+    return readAsStringAsync(path)
+  }
 }
