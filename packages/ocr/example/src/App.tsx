d5d2c4f70b068cf2a7a7a1ad44c8e97bd023a071
Guten Ye <ywzhaifei@gmail.com>
2024-05-08 14:19:24 +0800

    mv example

--- packages/ocr/example/src/App.tsx
+++ packages/ocr/example/src/App.tsx
@@ -1,31 +1,57 @@
-import * as React from 'react';
+import * as FileSystem from 'expo-file-system'
+import { useEffect, useState } from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import * as ocr from 'react-native-ocr'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { ImagePickerButton } from './ImagePickerButton'
+// import { recognize } from './recognize'
+import type { ImageDetails } from './types'
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-ocr';
+ocr.multiply(2, 3).then(console.log)
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [image, setImage] = useState<ImageDetails>()
+  const [resultText, setResultText] = useState<string>()
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  useEffect(() => {
+    ;(async function useEffect() {
+      if (!image) {
+        return
+      }
+      // const resultText = await recognize(image)
+      // setResultText(resultText)
+    })()
+  }, [image])
+
+  const handleChange = (image: ImageDetails) => {
+    setImage(image)
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.container}>
+          <ImagePickerButton onChange={handleChange} />
+          {image && <Image source={{ uri: image.uri }} style={styles.image} />}
+          {resultText && <Text>{resultText}</Text>}
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
 }
 
 const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  image: {
+    aspectRatio: 1,
+    width: '100%',
+    objectFit: 'contain',
   },
-});
+})
