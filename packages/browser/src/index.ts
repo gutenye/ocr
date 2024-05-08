4017b2ae81b76870498e5849277bbd91fb9a91d4
Guten Ye <ywzhaifei@gmail.com>
2024-05-08 10:07:26 +0800

    fix packages

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -1,9 +1,10 @@
 import Ocr, { registerBackend } from '@gutenye/ocr-common'
+import { splitIntoLineImages } from '@gutenye/ocr-common/splitIntoLineImages'
 import { InferenceSession } from 'onnxruntime-web'
 import { FileUtils } from './FileUtils'
 import { ImageRaw } from './ImageRaw'
 
-registerBackend({ FileUtils, ImageRaw, InferenceSession })
+registerBackend({ FileUtils, ImageRaw, InferenceSession, splitIntoLineImages })
 
 export * from '@gutenye/ocr-common'
 export default Ocr
