c0ed8bcf152ea1eeda89c9dd48f9f616f879a646
Guten Ye <ywzhaifei@gmail.com>
2024-05-08 15:24:10 +0800

    use expo example

---
+++ packages/react-native/src/index.tsx
@@ -0,0 +1,27 @@
+import { NativeModules, Platform } from 'react-native'
+
+const LINKING_ERROR =
+  `The package '@gutenye/react-native-ocr' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n'
+
+// @ts-expect-error
+const isTurboModuleEnabled = global.__turboModuleProxy != null
+
+const OcrModule = isTurboModuleEnabled ? require('./NativeOcr').default : NativeModules.Ocr
+
+const Ocr = OcrModule
+  ? OcrModule
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR)
+        },
+      },
+    )
+
+export function multiply(a: number, b: number): Promise<number> {
+  return Ocr.multiply(a, b)
+}
