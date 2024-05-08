8eb416553a912768cd96aceddb5f16774ed88eb7
Guten Ye <ywzhaifei@gmail.com>
2024-05-08 09:39:46 +0800

    use models package

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,11 +1,14 @@
 import Ocr, { registerBackend } from '@gutenye/ocr-common'
-import useDefaultModels from '@gutenye/ocr-common/use-default-models.node'
+import models from '@gutenye/ocr-models/node'
 import { InferenceSession } from 'onnxruntime-node'
 import { FileUtils } from './FileUtils'
 import { ImageRaw } from './ImageRaw'
 
-useDefaultModels()
 registerBackend({ FileUtils, ImageRaw, InferenceSession })
+
+Ocr.DEFAULT_DETECTION_PATH = models.detectionPath
+Ocr.DEFAULT_RECOGINTION_PATH = models.recognitionPath
+Ocr.DEFAULT_DICTIONARY_PATH = models.dictionaryPath
 
 export * from '@gutenye/ocr-common'
 export default Ocr
