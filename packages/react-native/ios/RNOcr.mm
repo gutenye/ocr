9b4601e2c81deb5df6eb950380d4957b8dc43d15
Guten Ye <ywzhaifei@gmail.com>
2024-05-11 20:20:16 +0800

    class1

--- packages/react-native/ios/RNOcr.mm
+++ packages/react-native/ios/RNOcr.mm
@@ -1,8 +1,5 @@
 #import "RNOcr.h"
 #import <onnxruntime_cxx_api.h>
-// #import <paddle_api.h>
-// #import <paddle_use_kernels.h>
-// #import <paddle_use_ops.h>
 #import <iostream>
 #import <opencv2/opencv.hpp>
 #import <string>
@@ -12,32 +9,37 @@
 @implementation RNOcr
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(ocr
-                  : (NSString *)img_path1 resolve
-                  : (RCTPromiseResolveBlock)resolve reject
-                  : (RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(create : (RCTPromiseResolveBlock)resolve rejecter : (RCTPromiseRejectBlock)reject) {
+  RNOcr *instance = [RNOcr new];
+
   NSString *path = [[NSBundle mainBundle] bundlePath];
   std::string paddle_dir = std::string([path UTF8String]);
-  // std::string det_model_file =
-  //     paddle_dir + "/resources.bundle/ch_ppocr_mobile_v2.0_det_slim_opt.nb";
-  // std::string rec_model_file =
-  //     paddle_dir + "/resources.bundle/ch_ppocr_mobile_v2.0_rec_slim_opt.nb";
-  // std::string cls_model_file =
-  //     paddle_dir + "/resources.bundle/ch_ppocr_mobile_v2.0_cls_slim_opt.nb";
-  std::string det_model_file = paddle_dir + "/resources.bundle/ch_PP-OCRv4_det_infer.onnx";
-  std::string rec_model_file = paddle_dir + "/resources.bundle/ch_PP-OCRv4_rec_infer.onnx";
-  std::string cls_model_file = paddle_dir + "/resources.bundle/ch_ppocr_mobile_v2.0_cls_infer.onnx";
-  std::string img_path = paddle_dir + "/resources.bundle/cn-01.jpg";
-  std::string output_img_path = paddle_dir + "/test_result.jpg";
-  std::string dict_path = paddle_dir + "/resources.bundle/ppocr_keys_v1.txt";
-  std::string config_path = paddle_dir + "/resources.bundle/config.txt";
+  std::string det_model_file = paddle_dir + "/gutenye-ocr-react-native.bundle/ch_PP-OCRv4_det_infer.onnx";
+  std::string rec_model_file = paddle_dir + "/gutenye-ocr-react-native.bundle/ch_PP-OCRv4_rec_infer.onnx";
+  std::string cls_model_file = paddle_dir + "/gutenye-ocr-react-native.bundle/ch_ppocr_mobile_v2.0_cls_infer.onnx";
+  std::string dict_path = paddle_dir + "/gutenye-ocr-react-native.bundle/ppocr_keys_v1.txt";
+  std::string config_path = paddle_dir + "/gutenye-ocr-react-native.bundle/config.txt";
 
-  std::cout << det_model_file << std::endl;
+  std::string img_path = paddle_dir + "/gutenye-ocr-react-native.bundle/cn-01.jpg";
+  // std::string output_img_path = paddle_dir + "/test_result.jpg";
 
-  NativeOcr *pipe_ =
+  instance.ocr =
       new NativeOcr(det_model_file, cls_model_file, rec_model_file, "LITE_POWER_HIGH", 1, config_path, dict_path);
+
+  resolve([self objectRefForInstance:instance]);
+}
+
+RCT_EXPORT_METHOD(detect
+                  : (NSString *)image_path options
+                  : (NSDictionary *)options
+
+                      resolve
+                  : (RCTPromiseResolveBlock)resolve reject
+                  : (RCTPromiseRejectBlock)reject) {
+  RNOcr *instance = (RNOcr *)(void *)[objectRef integerValue];
+
   std::vector<std::string> res_txt;
-  pipe_->Process(img_path, output_img_path, res_txt);
+  instance.ocr->Process(img_path, output_img_path, res_txt);
 
   std::ostringstream result;
   for (int i = 0; i < res_txt.size() / 2; i++) {
@@ -49,6 +51,10 @@ RCT_EXPORT_METHOD(ocr
   NSLog(@"%@", text);
 
   resolve(text);
+}
+
+- (NSNumber *)objectRefForInstance:(id)instance {
+  return @((NSInteger)instance);
 }
 
 // Don't compile this code when we build for the old architecture.
