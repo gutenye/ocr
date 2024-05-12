2e897f61061a8362b3060aa511ccf0e5f9ad63a7
Guten Ye <ywzhaifei@gmail.com>
2024-05-12 09:01:38 +0800

    convertNSDictionary work

--- packages/react-native/example/src/App.tsx
+++ packages/react-native/example/src/App.tsx
@@ -9,7 +9,14 @@ import type { ImageDetails } from './types'
 
 async function main() {
   const ocr = await Ocr.create()
-  const result = await ocr.detect(`${FileSystem.bundleDirectory}/gutenye-ocr-react-native.bundle/cn-01.jpg`)
+  const result = await ocr.detect(`${FileSystem.bundleDirectory}/gutenye-ocr-react-native.bundle/cn-01.jpg`, {
+    number1: 1,
+    number2: 0,
+    float1: 1.1,
+    string: 'a',
+    boolean1: true,
+    boolean2: false,
+  })
   console.log('js', result)
   return result
 }
