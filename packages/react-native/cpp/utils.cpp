4f132a13f1da8a6eda9128704b18b742db779bf7
Muhamad Rizki <muhrizkie88@gmail.com>
2024-10-10 05:21:11 +0700

    fix: remove arm_neon.h

--- packages/react-native/cpp/utils.cpp
+++ packages/react-native/cpp/utils.cpp
@@ -13,65 +13,33 @@
 // limitations under the License.
 
 #include "utils.h"
-#include <arm_neon.h>
 #include <iostream>
+#include <vector>
 
 void NHWC3ToNC3HW(const float *din, float *dout, int size, const std::vector<float> mean,
                   const std::vector<float> scale) {
-  if (mean.size() != 3 || scale.size() != 3) {
-    std::cerr << "[ERROR] mean or scale size must equal to 3\n" << std ::endl;
-    return;
-  }
-  float32x4_t vmean0 = vdupq_n_f32(mean[0]);
-  float32x4_t vmean1 = vdupq_n_f32(mean[1]);
-  float32x4_t vmean2 = vdupq_n_f32(mean[2]);
-  float32x4_t vscale0 = vdupq_n_f32(scale[0]);
-  float32x4_t vscale1 = vdupq_n_f32(scale[1]);
-  float32x4_t vscale2 = vdupq_n_f32(scale[2]);
+    if (mean.size() != 3 || scale.size() != 3) {
+        std::cerr << "[ERROR] mean or scale size must equal to 3\n" << std::endl;
+        return;
+    }
 
-  float *dout_c0 = dout;
-  float *dout_c1 = dout + size;
-  float *dout_c2 = dout + size * 2;
+    float *dout_c0 = dout;
+    float *dout_c1 = dout + size;
+    float *dout_c2 = dout + size * 2;
 
-  int i = 0;
-  for (; i < size - 3; i += 4) {
-    float32x4x3_t vin3 = vld3q_f32(din);
-    float32x4_t vsub0 = vsubq_f32(vin3.val[0], vmean0);
-    float32x4_t vsub1 = vsubq_f32(vin3.val[1], vmean1);
-    float32x4_t vsub2 = vsubq_f32(vin3.val[2], vmean2);
-    float32x4_t vs0 = vmulq_f32(vsub0, vscale0);
-    float32x4_t vs1 = vmulq_f32(vsub1, vscale1);
-    float32x4_t vs2 = vmulq_f32(vsub2, vscale2);
-    vst1q_f32(dout_c0, vs0);
-    vst1q_f32(dout_c1, vs1);
-    vst1q_f32(dout_c2, vs2);
-
-    din += 12;
-    dout_c0 += 4;
-    dout_c1 += 4;
-    dout_c2 += 4;
-  }
-  for (; i < size; i++) {
-    *(dout_c0++) = (*(din++) - mean[0]) * scale[0];
-    *(dout_c1++) = (*(din++) - mean[1]) * scale[1];
-    *(dout_c2++) = (*(din++) - mean[2]) * scale[2];
-  }
+    for (int i = 0; i < size; i++) {
+        dout_c0[i] = (din[i * 3 + 0] - mean[0]) * scale[0];
+        dout_c1[i] = (din[i * 3 + 1] - mean[1]) * scale[1];
+        dout_c2[i] = (din[i * 3 + 2] - mean[2]) * scale[2];
+    }
 }
 
 void NHWC1ToNC1HW(const float *src, float *dst, const float *mean, const float *std, int width, int height) {
-  int size = height * width;
-  float32x4_t vmean = vdupq_n_f32(mean ? mean[0] : 0.0f);
-  float32x4_t vscale = vdupq_n_f32(std ? (1.0f / std[0]) : 1.0f);
-  int i = 0;
-  for (; i < size - 3; i += 4) {
-    float32x4_t vin = vld1q_f32(src);
-    float32x4_t vsub = vsubq_f32(vin, vmean);
-    float32x4_t vs = vmulq_f32(vsub, vscale);
-    vst1q_f32(dst, vs);
-    src += 4;
-    dst += 4;
-  }
-  for (; i < size; i++) {
-    *(dst++) = (*(src++) - mean[0]) / std[0];
-  }
+    int size = height * width;
+    float vmean = (mean ? mean[0] : 0.0f);
+    float vscale = (std ? (1.0f / std[0]) : 1.0f);
+
+    for (int i = 0; i < size; i++) {
+        dst[i] = (src[i] - vmean) * vscale;
+    }
 }
