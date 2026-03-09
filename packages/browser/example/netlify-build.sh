85c4e5d2a3d8f27332be587cbf1cf93b4072ec69
Alex <alex.caraba@technest.es>
2026-03-09 03:48:56 +0100

    fix: build packages before example in netlify

--- packages/browser/example/netlify-build.sh
+++ packages/browser/example/netlify-build.sh
@@ -2,6 +2,18 @@
 
 export DEFAULT_IMAGE_PATH='/assets/cn-01.jpg'
 
+# Build the common package first (dependency)
+cd ../../common
+bun run tsc --project tsconfig.build.json
+bun run tsc-alias --project tsconfig.build.json
+
+# Build the browser package
+cd ../browser
+bun run tsc --project tsconfig.build.json
+bun run tsc-alias --project tsconfig.build.json
+
+# Build the example
+cd example
 bun run build.ts
 
 cp -rL build build2
